c793a4a9bdb653b4545e0874957b62c0bbb67670
Cesar Forero <cesarjf18@hotmail.com>
2023-10-01 22:34:40 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ app = FastAPI()
 
 def index():
     return "esto es la prueba # 1"
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    options = ['Action', 'Casual', 'Indie', 'Simulation', 'Strategy',
+       'Free to Play', 'RPG', 'Sports', 'Adventure', 'Racing',
+       'Early Access', 'Massively Multiplayer',
+       'Animation &amp; Modeling', 'Web Publishing', 'Education',
+       'Software Training', 'Utilities', 'Design &amp; Illustration',
+       'Audio Production', 'Video Production', 'Photo Editing']  # Generate a list of 21 options
+    return render_template('index.html', options=options)
+
+if __name__ == '__main__':
+    app.run(debug=True)
