3bebae676ea9da8071801d0a2c978518a27e9354
Cesar Forero <cesarjf18@hotmail.com>
2023-10-02 16:53:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import pandas as pd
 
 app = FastAPI()
+df_merged = pd.read_csv("df_merged.csv")
 
 #http://127.0.0.1:8000
 
@@ -24,9 +25,8 @@ def PlayTimeGenre(genero: str):
 
 @app.get('/UserForGenre/{genero}')
 def UserForGenre(genero: str):
-    df_merged = pd.read_csv("df_merged.csv")
     filtro_genero = df_merged[df_merged[genero] == True]
     grouped_df = filtro_genero.groupby('user_id')['playtime_forever'].max().reset_index()
     max_playtime = grouped_df['playtime_forever'].max()
     user_id_with_max_playtime = grouped_df.loc[grouped_df['playtime_forever'].idxmax(), 'user_id']
-    return {'Genero': genero,'Tiempo de juego': max_playtime,'Usuario': user_id_with_max_playtime}
+    return {'Genero': genero,'Tiempo de juego':max_playtime,'Usuario': user_id_with_max_playtime}
