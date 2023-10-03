e59ad9b3271628847e71c1f14c1b2b5b2d6bb2fe
Cesar Forero <cesarjf18@hotmail.com>
2023-10-03 01:22:26 -0500

    cambios

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 import pandas as pd
-import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = FastAPI()
@@ -43,15 +42,15 @@ def UserForGenre(genero: str):
 def recomendacion_juego(id_de_producto):
     item_profiles = df_sample.groupby('item_id').agg({'sentiment_score': 'mean', 'item_name': 'first'}).reset_index()
     if item_profiles.empty:
-    print("No se encontró un juego similar")
+        print("No se encontró un juego similar")
     else:
-    similarity_matrix = cosine_similarity(item_profiles[['sentiment_score']])
-    item_index = item_profiles[item_profiles['item_id'] == id_de_producto].index
+        similarity_matrix = cosine_similarity(item_profiles[['sentiment_score']])
+        item_index = item_profiles[item_profiles['item_id'] == id_de_producto].index
     if item_index.empty:
-    print("No existe este juego.")
+        print("No existe este juego.")
     return [], []
-    item_index = item_index[0]
-    similar_items_indices = similarity_matrix[item_index].argsort()[::-1][1:6]
-    recommendations = sample_df.loc[similar_items_indices, 'item_id'].tolist()
-    recommendation_names = sample_df.loc[similar_items_indices, 'item_name'].tolist()
+        item_index = item_index[0]
+        similar_items_indices = similarity_matrix[item_index].argsort()[::-1][1:6]
+        recommendations = sample_df.loc[similar_items_indices, 'item_id'].tolist()
+        recommendation_names = sample_df.loc[similar_items_indices, 'item_name'].tolist()
     return recommendations, recommendation_names
